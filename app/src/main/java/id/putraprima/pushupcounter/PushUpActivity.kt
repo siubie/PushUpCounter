895200f3990e4d6ebe879fc61a8ebce4e580873a
Putra Prima Arhandi <putraprima@gmail.com>
2020-08-21 22:07:30 +0700

    ganti nama id

---
+++ app/src/main/java/id/putraprima/pushupcounter/PushUpActivity.kt
@@ -0,0 +1,11 @@
+package id.putraprima.pushupcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PushUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push_up)
+    }
+}
