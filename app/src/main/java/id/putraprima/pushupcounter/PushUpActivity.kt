afbf6600688836a9a748804f4952a43d036fdaf2
Putra Prima Arhandi <putraprima@gmail.com>
2020-09-07 15:43:01 +0700

    data binding start

--- app/src/main/java/id/putraprima/pushupcounter/PushUpActivity.kt
+++ app/src/main/java/id/putraprima/pushupcounter/PushUpActivity.kt
@@ -2,10 +2,23 @@ package id.putraprima.pushupcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class PushUpActivity : AppCompatActivity() {
+    var counter:Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_push_up)
+        val container = findViewById<ConstraintLayout>(R.id.container_pushup_counter)
+        val textCounter=findViewById<TextView>(R.id.text_pushup_count)
+        textCounter.text = "0"
+        container.setOnClickListener {
+            counter += 1
+            textCounter.text = counter.toString()
+        }
+
     }
+
 }
